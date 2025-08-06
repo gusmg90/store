cdccf9eb50228da0f382e31e4ff624f8e337b657
Gustavo Martinez <gustavo@grupobarco.mx>
2025-08-05 22:03:54 -0600

    clase 30

---
+++ src/app/domains/products/components/product/product.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product',
+  imports: [],
+  templateUrl: './product.html',
+  styleUrl: './product.css'
+})
+export class Product {
+img = 'https://picsum.photos/200/300?random=1';
+}
